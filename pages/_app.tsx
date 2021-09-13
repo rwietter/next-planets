a64d920d73e0f0944ccb7b2e1e93013e5f7a3ae6
rwietter <mauriciobw17@gmail.com>
2021-09-13 11:34:25 -0300

    chore: fix eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import type { AppProps } from 'next/app'
+/* eslint-disable react/jsx-props-no-spreading */
+
+import type { AppProps } from 'next/app';
 import '../styles/fonts.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
